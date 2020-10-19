0125456f7921637e58794f599633bc857f59dc2a
= <=>
2020-10-19 15:59:22 +0700

    Initial Commit

---
+++ report/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from report.models import *
+
+from .DBHelper import DBHelper
+
+# Create your views here.
+def index(request):
+    invoice_no = request.GET.get('inv','')
+    data_report = dict()
+    data_report['invoice'] = list(Invoice.objects.filter(invoice_no=invoice_no).select_related('customer_code').values('invoice_no', 'date', 'customer_code_id', 'customer_code__name','due_date','total','vat','amount_due'))
+    data_report['invoice_line_item'] = list(InvoiceLineItem.objects.filter(invoice_no=invoice_no).values())
+    #return JsonResponse(data_report)
+    return render(request, 'report_data.html', data_report)
+
+def ReportListAllInvoices(request):
+    db = DBHelper()
+    data, columns = db.fetch ('SELECT i.invoice_no as "Invoice No", i.date as "Date" '
+                            ' , i.customer_code as "Customer Code", c.name as "Customer Name" '
+                            ' , i.due_date as "Due Date", i.total as "Total", i.vat as "VAT", i.amount_due as "Amount Due" '
+                            ' , ili.product_code as "Product Code", p.name as "Product Name" '
+                            ' , ili.quantity as "Quantity", ili.unit_price as "Unit Price", ili.extended_price as "Extended Price" '
+                            ' FROM invoice i JOIN customer c ON i.customer_code = c.customer_code '
+                            '  JOIN invoice_line_item ili ON i.invoice_no = ili.invoice_no '
+                            '  JOIN product p ON ili.product_code = p.code '
+                            ' ')
+    data_report = dict()
+    data_report['data'] = CursorToDict (data,columns)
+    data_report['column_name'] = columns
+
+    return render(request, 'report_list_all_invoices.html', data_report)
+
+def ReportProductsSold(request):
+    db = DBHelper()
+    data, columns = db.fetch ('SELECT ili.product_code as "Product Code", p.name as "Product Name" '
+                              ' , SUM(ili.quantity) as "Total Quantity Sold", SUM(ili.extended_price) as "Total Value Sold" '
+                              ' FROM invoice i JOIN invoice_line_item ili ON i.invoice_no = ili.invoice_no '
+                              '   JOIN product p ON ili.product_code = p.code '
+                              ' GROUP BY p.code, ili.product_code, p.name '
+                            ' ')
+    data_report = dict()
+    data_report['data'] = CursorToDict (data,columns)
+    data_report['column_name'] = columns
+
+    return render(request, 'report_products_sold.html', data_report)
+
+def ReportListAllProducts(request):
+    db = DBHelper()
+    data, columns = db.fetch ('SELECT code as "Code", name as "Name", units as "Units" FROM product '
+                              ' ')
+    data_report = dict()
+    data_report['data'] = CursorToDict (data,columns)
+    data_report['column_name'] = columns
+
+    return render(request, 'report_list_all_products.html', data_report)
+
+def CursorToDict(data,columns):
+    result = []
+    fieldnames = [name.replace(" ", "_").lower() for name in columns]
+    for row in data:
+        rowset = []
+        for field in zip(fieldnames, row):
+            rowset.append(field)
+        result.append(dict(rowset))
+    return result
