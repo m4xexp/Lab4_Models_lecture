db9a5f165b6aefbd92133b2d75ff18b3f8dc2c9e
m4xexp <parames.m4xexp@gmail.com>
2020-10-25 23:41:23 +0700

    Semi Final

--- report/views.py
+++ report/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 
-from report.models import *
-
 from .DBHelper import DBHelper
 
 def index(request):
@@ -13,25 +11,18 @@ def index(request):
 def ReportListAllReceipts(request):
     db = DBHelper()
     data, columns = db.fetch ('SELECT r.receipt_no as "Receipt No", r.date as "Date" '
-                              ' , r.customer_code as "Customer Code", c.name as "Customer Name", r.payment_code as "Payment Method", r.payment_ref as "Payment Reference" '
+                              ' , r.customer_code as "Customer Code", c.name as "Customer Name" '
+                              ' , r.payment_code as "Payment Method", r.payment_ref as "Payment Reference" '
                               ' , r.remarks as "Remarks", r.total_received as "Total Received"'
                               ' , rli.invoice_no as "Invoice No"'
                               ' , i.date as "Invoice Date", rli.amount_paid_here as "Amount Paid Here"'
                               '  FROM receipt r '
                               '  JOIN customer c ON r.customer_code = c.customer_code '
                               '  JOIN receipt_line_item rli ON r.receipt_no = rli.receipt_no '
-                              '  JOIN invoice i ON rli.invoice_no = i.invoice_no ' )
-
-    data1, columns1 = db.fetch('SELECT rli.invoice_no as "Invoice No"'
-                              ' , i.date as "Invoice Date", rli.amount_paid_here as "Amount Paid Here"'
-                              '  FROM receipt r '
-                              '  JOIN customer c ON r.customer_code = c.customer_code '
-                              '  JOIN receipt_line_item rli ON r.receipt_no = rli.receipt_no '
-                              '  JOIN invoice i ON rli.invoice_no = i.invoice_no ' )
+                              '  JOIN invoice i ON rli.invoice_no = i.invoice_no ')
 
     data_report = dict()
     data_report['data'] = CursorToDict (data,columns)
-    data_report['data1'] = CursorToDict (data1,columns1)
     data_report['column_name'] = columns
 
     return render(request, 'report_list_all_receipts.html', data_report)
@@ -63,15 +54,23 @@ def ReportUnpaidInvoices(request):
 
     return render(request, 'report_unpaid_invoice.html', data_report)
 
-def ReportProductsSold(request):
+def ReportDateAmountReceived(request):
     db = DBHelper()
-    data, columns = db.fetch ('SELECT code as "Code", name as "Name", units as "Units" FROM product '
-                              ' ')
+    data, columns = db.fetch (' SELECT "Date", "Customer Name", "Invoice Amount Received" '
+                              ' FROM(SELECT i.date as "Date", c.customer_code as "Customer Code", c.name as "Customer Name", '
+		                      ' i.invoice_no as "Invoice No","Invoice Amount Received" '
+		                      ' FROM (SELECT rli.invoice_no as "Invoice No", SUM(rli.amount_paid_here) as "Invoice Amount Received" '
+	  		                  ' FROM receipt_line_item as rli '
+	                          ' GROUP BY rli.invoice_no ) as li '
+			                  ' JOIN invoice as i '
+	                          ' ON li."Invoice No" = i.invoice_no '
+                              ' JOIN customer as c '
+	                          ' ON i.customer_code = c.customer_code) as CD ')
     data_report = dict()
     data_report['data'] = CursorToDict (data,columns)
     data_report['column_name'] = columns
 
-    return render(request, 'report_list_all_products.html', data_report)
+    return render(request, 'report_date_amount_received.html', data_report)
 
 def CursorToDict(data,columns):
     result = []
