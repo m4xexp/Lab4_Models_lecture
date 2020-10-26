b9c7687ab2ec4908b23b079269c23cbcc7549716
m4xexp <parames.m4xexp@gmail.com>
2020-10-26 22:10:15 +0700

    NongNang

--- report/views.py
+++ report/views.py
@@ -19,7 +19,7 @@ def ReportListAllReceipts(request):
                               ' FROM receipt r '
                               ' JOIN customer c ON r.customer_code = c.customer_code '
                               ' JOIN receipt_line_item rli ON r.receipt_no = rli.receipt_no '
-                              ' JOIN invoice i ON rli.invoice_no = i.invoice_no ')
+                              ' JOIN invoice i ON rli.invoice_no = i.invoice_no')
 
     data_report = dict()
     data_report['data'] = CursorToDict (data,columns)
@@ -36,34 +36,31 @@ def ReportUnpaidInvoices(request):
 	                          ' FROM receipt_line_item as rli '
 	                          ' GROUP BY rli.invoice_no ) as li '
                               ' JOIN invoice as i ON li."Invoice No" = i.invoice_no '
-                              ' JOIN customer as c ON i.customer_code = c.customer_code '
-                              '')
+                              ' JOIN customer as c ON i.customer_code = c.customer_code')
 
-    data1, columns1 = db.fetch (' SELECT COUNT(li) as "List", SUM(i.amount_due - "Invoice Amount Received") AS "Total Dept" '
+    data1, columns1 = db.fetch (' SELECT COUNT(li) as "Total", SUM(i.amount_due - "Invoice Amount Received") AS "Total Unpaid Invoice" '
                                 ' FROM(SELECT rli.invoice_no AS "Invoice No", SUM(rli.amount_paid_here) as "Invoice Amount Received" '
 	                            ' FROM receipt_line_item as rli '
 	                            ' GROUP BY rli.invoice_no ) as li '
-                                ' JOIN invoice as i ON li."Invoice No" = i.invoice_no '
-                                '')
+                                ' JOIN invoice as i ON li."Invoice No" = i.invoice_no')
     
     data_report = dict()
     data_report['data'] = CursorToDict (data,columns)
     data_report['data1'] = CursorToDict (data1,columns1)
     data_report['column_name'] = columns
+    data_report['column_name1'] = columns1
 
     return render(request, 'report_unpaid_invoice.html', data_report)
 
 def ReportDateProductSold(request):
     db = DBHelper()
-    data, columns = db.fetch (' SELECT i.date as "Date", p.code as "Product Code", p.name as "Product Name", '
+    data, columns = db.fetch (' SELECT i.date as "Date", p.name as "Product Name", '
                               ' SUM(ili.quantity) as "Total Sold", p.units as "Unit", '
 		                      ' SUM(ili.extended_price) as "Total Extended Price"  '
                               ' FROM product p JOIN invoice_line_item ili ON p.code = ili.product_code '
 		                      ' JOIN invoice i ON i.invoice_no = ili.invoice_no '
 	                          ' GROUP BY i.date,p.code,p.name,p.units '
-                              ' ORDER BY i.date ASC ' 
-                              '')
-
+                              ' ORDER BY i.date ASC ')
 
     data_report = dict()
     data_report['data'] = CursorToDict (data,columns)
